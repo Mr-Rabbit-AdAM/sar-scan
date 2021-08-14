cc7704f1e67bda3f5451d4436bc23add816d4ac5
Ad-AM <87221134+BCKS-AdAM@users.noreply.github.com>
2021-08-14 22:16:14 +0630

    Create sar.sh

---
+++ sar.sh
@@ -0,0 +1,33 @@
+#!/din/bash
+clear 
+
+echo ""
+
+echo -e "\e[1;32m   Sar scan tool \e[0m "
+
+echo ""
+
+echo -e "\e[1;32m     only one for cho cho "
+
+echo -e " 1. Start "
+
+read -p "Ad A M @ >> : " ch 
+
+if [[ $ch == 1 ]];then 
+
+echo -e "\e[1;32m  10% "
+sleep 4 
+echo -e "\e[1;32m  30% "
+sleep 3 
+echo -e "\e[1;32m  45% " 
+sleep 5 
+echo -e "\e[1;32m  75% "
+sleep 2 
+echo -e "\e[1;32m  85% " 
+sleep 3 
+echo -e "\e[1;32m  100% "
+
+echo "" 
+
+echo -e "\e[1;31m There is no girl for you"
+fi
